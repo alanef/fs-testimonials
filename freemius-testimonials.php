d66e3a04a972dd90f615cb6c7bbe44748d873a4f
Shramee Srivastav <shramee.srivastav@gmail.com>
2017-09-29 16:51:05 +0530

    Singleton and admin area

--- freemius-testimonials.php
+++ freemius-testimonials.php
@@ -11,12 +11,68 @@ Domain: fs-testimonial
 
 class FS_Testimonials {
 
+	private static $_instance;
+
+	public static function instance() {
+		if ( ! self::$_instance ) {
+			self::$_instance = new self();
+		}
+
+		return self::$_instance;
+	}
+
 	/**
 	 * FS_Testimonials constructor.
 	 */
 	public function __construct() {
 		add_shortcode( 'freemius-testimonials', [ $this, 'testimonials' ] );
 
+		add_action( 'admin_init', [ $this, 'admin' ] );
+	}
+
+	public function admin() {
+
+		register_setting( 'general', 'fstm_credentials' );
+
+		add_settings_section(
+			'fstm_general_section',
+			__( 'Freemius testimonials', 'fs-testimonial' ),
+			[ $this, 'section_render' ],
+			'general'
+		);
+
+	}
+
+	public function section_render() {
+		$settings = get_option( 'fstm_credentials', [] );
+
+		$settings = wp_parse_args( $settings, [
+			'dev_id'     => '',
+			'dev_public' => '',
+			'dev_secret' => '',
+		] );
+		?>
+		<table class="form-table">
+			<tr>
+				<th scope="row"><?php _e( 'Developer ID', 'fs-testimonial' ) ?></th>
+				<td>
+					<input class="regular-text" type="text" name='fstm_credentials[dev_id]' value="<?php echo $settings['dev_id'] ?>">
+				</td>
+			</tr>
+			<tr>
+				<th scope="row"><?php _e( 'Developer Public Key', 'fs-testimonial' ) ?></th>
+				<td>
+					<input class="regular-text" type="text" name='fstm_credentials[dev_public]' value="<?php echo $settings['dev_public'] ?>">
+				</td>
+			</tr>
+			<tr>
+				<th scope="row"><?php _e( 'Developer Secret Key', 'fs-testimonial' ) ?></th>
+				<td>
+					<input class="regular-text" type="text" name='fstm_credentials[dev_secret]' value="<?php echo $settings['dev_secret'] ?>">
+				</td>
+			</tr>
+		</table>
+		<?php
 	}
 
 	/**
@@ -30,4 +86,4 @@ class FS_Testimonials {
 
 }
 
-new FS_Testimonials();
+FS_Testimonials::instance();
